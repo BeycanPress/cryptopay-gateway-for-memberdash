f8968f1600379049c6a18b7a5efd56edab4e7cb1
BeycanDeveloper <halilbeycan0@gmail.com>
2024-11-28 14:31:34 +0300

    fixed text domain warning

--- cryptopay-gateway-for-memberdash.php
+++ cryptopay-gateway-for-memberdash.php
@@ -11,7 +11,7 @@ defined('ABSPATH') || exit;
 
 /**
  * Plugin Name: CryptoPay Gateway for MemberDash
- * Version:     1.0.1
+ * Version:     1.0.2
  * Plugin URI:  https://beycanpress.com/cryptopay/
  * Description: Adds Cryptocurrency payment gateway (CryptoPay) for MemberDash.
  * Author:      BeycanPress LLC
@@ -31,7 +31,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use BeycanPress\CryptoPay\Integrator\Helpers;
 
 define('MD_CRYPTOPAY_FILE', __FILE__);
-define('MD_CRYPTOPAY_VERSION', '1.0.1');
+define('MD_CRYPTOPAY_VERSION', '1.0.2');
 define('MD_CRYPTOPAY_URL', plugin_dir_url(__FILE__));
 define('MD_CRYPTOPAY_DIR', plugin_dir_path(__FILE__));
 
@@ -46,11 +46,13 @@ function memberdash_cryptopay_addModels(): void
 
 memberdash_cryptopay_addModels();
 
+add_action('init', function (): void {
+    load_plugin_textdomain('cryptopay-gateway-for-memberdash', false, basename(__DIR__) . '/languages');
+});
+
 add_action('plugins_loaded', function (): void {
 
     memberdash_cryptopay_addModels();
-
-    load_plugin_textdomain('cryptopay-gateway-for-memberdash', false, basename(__DIR__) . '/languages');
 
     if (!defined('MEMBERDASH_VERSION')) {
         Helpers::requirePluginMessage('MemberDash', 'https://www.learndash.com/memberdash-plugin/', false);
